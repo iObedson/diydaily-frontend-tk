fce3e3fa74f3b3686635ef23e62d36a264feae06
Nob-dev <mobmoney101@gmail.com>
2022-10-31 11:19:50 +0100

    next.config.js added

---
+++ next.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  publicRuntimeConfig: {
+    APP_NAME: 'DIY Daily',
+    API_DEVELOPMENT: 'https://pear-clumsy-nightingale.cyclic.app/api',
+    API_PRODUCTION: 'https://pear-clumsy-nightingale.cyclic.app/api',
+    DOMAIN_DEVELOPMENT: 'http://localhost:3000',
+    DOMAIN_PRODUCTION: 'https://diydaily-tk.netlify.app',
+    FB_APP_ID: '627061882313745',
+    DISQUS_SHORTNAME: 'diydaily',
+    GOOGLE_CLIENT_ID:
+      '617582187629-69qmnungf11sfvadb48j1i5nk8tl9h07.apps.googleusercontent.com',
+  },
+};
